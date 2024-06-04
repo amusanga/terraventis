70b5ad60cd1fcedbc895ce181f991391997640e5
amusanga <a.cesaire07@gmail.com>
2024-06-04 20:40:34 +0200

    Added code to compute indexes

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # terraventis
+
+CRIRPS dataset for precipitation
+climate dataset for temperature and evopo transpiration 
+
+
+
+based on soil property, indexes changed but other parameters are the same. 
--- experiment.py
+++ experiment.py
@@ -2,7 +2,7 @@ import rasterio
 import numpy as np
 
 # Open the downloaded GeoTIFF file
-with rasterio.open('home/cesaire/Downloads/field_image.tif') as src:
+with rasterio.open('/home/cesaire/Projects/terraventis/LANDSAT_IMAGES/Landsat_2024-05-16_11-00-00.tif') as src:
     # Read the image as a NumPy array
     array = src.read(1)  # Reads the first band
 
